e1c581be8516eb97ae17e0a40a3cdbdbbcbda6cd
Duggu-2005 <devanshumishra0204s@gmail.com>
2025-12-06 23:42:57 +0530

    Update settings.py again

--- breast/settings.py
+++ breast/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=kpmvumws3hkg#ak=+x-jc1mq1#4q!%*^)amcbc=v=cz&4b1+f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =
+ALLOWED_HOSTS =[
     'localhost',
     '127.0.0.1',
     'ai-powered-cancer-detection-system.onrender.com',
